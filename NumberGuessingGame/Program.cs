22132a9a9f106b02921f76a506f267755a9825c2
sjojon <90796535+sjojon@users.noreply.github.com>
2023-05-10 11:13:01 +0200

    Update Program.cs

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using static System.Console;
 
 namespace NumberGuessingGame
 {
@@ -8,32 +9,32 @@ namespace NumberGuessingGame
         {
             Random random = new();
             int randomNumber = random.Next(1, 101);
-            Console.WriteLine("Guess a number between 1 and 100");
+            WriteLine("Guess a number between 1 and 100");
             //Console.WriteLine(randomNumber); // TODO: Husk å fjerne denne linja!!
 
             while (true)
             {
-                Console.WriteLine("Write your number:");
-                string? input = Console.ReadLine();
+                WriteLine("Write your number:");
+                string? input = ReadLine();
                 if (int.TryParse(input, out int guess))
                 {
                     if (guess > randomNumber)
                     {
-                        Console.WriteLine("The number you guessed is too high!");
+                        WriteLine("The number you guessed is too high!");
                     }
                     else if (guess < randomNumber)
                     {
-                        Console.WriteLine("The number you guessed is too low!");
+                        WriteLine("The number you guessed is too low!");
                     }
                     else if (guess == randomNumber)
                     {
-                        Console.WriteLine("Congratulations, You guessed the right number!");
+                        WriteLine("Congratulations, You guessed the right number!");
                         break;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                    WriteLine("Invalid input. Please enter a valid number.");
                 }
             }
         }
